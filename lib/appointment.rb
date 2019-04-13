5e9f9b7891d91ba02d8b5d03ee9da0af065247a8
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-13 23:42:32 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,10 +1,15 @@
 class Appointment
   attr_accessor :date, :patient, :doctor
+  @@all=[]
 
   def initialize(patient, date)
     @patient=patient
     @date=date
     @@all<<self
+  end
+
+  def self.all
+    @@all
   end
 
 end
