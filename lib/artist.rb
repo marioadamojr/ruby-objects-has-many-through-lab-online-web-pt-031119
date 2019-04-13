e70366c38c9798be0c801e721029936683acbe1d
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-13 23:58:54 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,29 @@
 class Artist
   attr_accessor :name
-
   @@all = []
 
-  def initialize
+  def initialize(name)
+    @name=name
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def new_song(song_name, song_genre)
+    song = Song.new(song_name, self, song_genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    Song.all.collect do |song|
+      song.genre
+    end
   end
 end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,2 +1,29 @@
 class Patient
+  attr_accessor :name
+  @@all=[]
+
+  def initialize(name)
+    @name=name
+    @@all<<self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(doctor, date)
+    appointment = Appointment.new(self, doctor, date)
+  end
+
+  def appointments
+    Appointment.all.select do |app|
+      app.patient == self
+    end
+  end
+
+  def doctors
+    Appointment.all.collect do |app|
+      app.doctor
+    end
+  end
 end
