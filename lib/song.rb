5e0c4513e32bdcd5e33d44cf80ee1be7b9bc48f8
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-13 23:15:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,10 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@all = []
-  def initialize(name, artist, genre)
+  def initialize(name) #, artist, genre)
     @name=name
-    @artist=artist
-    @genre=genre
+    # @artist=artist
+    # @genre=genre
     @@all << self
   end
 
