f5d0fa72cd05897b500f14437203a56ad83a574c
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-13 23:44:35 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
   end
 
   def new_appointment(date, patient)
-    appointment = Appointment.new(date, self, patient)
+    appointment = Appointment.new(patient, self, date)
   end
 
   def appointments
