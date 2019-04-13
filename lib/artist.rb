de03592fbe6fcf157c9e72911c0f14609fca5a1b
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-13 23:13:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,9 +13,7 @@ class Artist
   end
 
   def new_song(song_name, song_genre)
-    song = Song.new(song_name)
-    song.genre=song_genre
-    song.artist=self
+    song = Song.new(song_name, self, song_genre)
   end
 
   def songs
